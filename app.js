af1666cbf3362509d03dee4c4dce6ecc9a3d973e
Eduardo Cajaiba <cajaibaeduardo@gmail.com>
2024-09-06 13:20:29 -0300

    Projeto optimizado
    Agora o campo reconhece os valores inseridos.

--- app.js
+++ app.js
@@ -1,23 +1,49 @@
 function pesquisar() {
-    // Obtém a seção HTML onde os resultados serão exibidos
+    // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
-
-    // Inicializa uma string vazia para armazenar o HTML dos resultados
-    let resultado = "";
   
-    // Itera sobre cada dado no array de dados
-    for (let dado of dados) {
-      // Constrói o HTML para cada item, formatando com as propriedades do dado
-      resultado += `
-        <div class="item-resultado">
-          <h2>${dado.titulo}</h2>
-          <p class="descricao-meta">${dado.descricao}</p>
-          <a href=${dado.link} target="_blank">Mais informações</a>
-        </div>
-      `;
+    // Obtém o valor digitado no campo de pesquisa e converte para minúsculas
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.toLowerCase();
+  
+    // Verifica se o campo de pesquisa está vazio
+    if (!campoPesquisa) {
+      // Se estiver vazio, exibe uma mensagem de erro
+      section.innerHTML = "<p>Nenhum resultado encontrado. Tente digitar o nome do atleta ou esporte.</p>";
+      return;
     }
   
-    // Atualiza o conteúdo da seção com o HTML construído
+    // Inicializa variáveis para armazenar o resultado da pesquisa
+    let resultado = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+  
+    // Itera sobre os dados da pesquisa
+    for (let dado of dados) {
+      // Converte o título, descrição e tags para minúsculas para facilitar a comparação
+      titulo = dado.titulo.toLowerCase();
+      descricao = dado.descricao.toLowerCase();
+      tags = dado.tags.toLowerCase();
+  
+      // Verifica se a pesquisa ocorre em algum dos campos
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        // Se encontrar um resultado, adiciona o HTML para exibi-lo na página
+        resultado += `
+          <div class="item-resultado">
+            <h2>${dado.titulo}</h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href=${dado.link} target="_blank">Mais informações</a>
+          </div>
+        `;
+      }
+    }
+  
+    // Se nenhum resultado for encontrado, exibe uma mensagem
+    if (!resultado) {
+      resultado = "<p>Nenhum resultado encontrado</p>";
+    }
+  
+    // Atualiza o conteúdo da seção com os resultados da pesquisa
     section.innerHTML = resultado;
   }
 
--- dados.js
+++ dados.js
@@ -1,17 +1,32 @@
 let dados = [
     {
-        titulo : "Rayssa Leal",
-        descricao : "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu talento e carisma, ela conquistou fãs ao redor do mundo.",
-        link : "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/jhulia-rayssa-mendes-leal",
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista profissional brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com seu talento e carisma, ela conquistou fãs ao redor do mundo.",
+        link: "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/jhulia-rayssa-mendes-leal",
+        tags: "skate fada sk8 bronze",
     },
     {
         titulo : "Rebeca Andrade",
         descricao : "Rebeca Andrade é, sem dúvida, um dos maiores nomes da ginástica artística brasileira. Com um talento inegável e uma determinação ímpar, ela conquistou o coração dos brasileiros e se tornou um verdadeiro símbolo de superação.",
         link : "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/rebeca-rodrigues-de-andrade",
+        tags: "ginastica salto ginasta solo ouro prata",
     },
     {
-        titulo : "Beatriz Sousa",
+        titulo : "Beatriz Souza",
         descricao : "Beatriz Souza é uma judoca brasileira que tem se destacado no cenário internacional e se consolidado como uma das grandes promessas do esporte no país. Com um talento nato e uma dedicação impressionante, ela vem conquistando diversas medalhas e títulos importantes, colocando o Brasil no topo do pódio em diversas competições.",
         link : "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/atleta",
-    }
+        tags: "judô judo judoka ouro",
+    },
+    {
+        titulo: "Caio Bonfim",
+        descricao: "Caio Bonfim é um marchador atlético brasileiro que conquistou a medalha de prata nos Jogos Olímpicos de Paris 2024, demonstrando grande técnica e resistência.",
+        link: "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/caio-bonfim",
+        tags: "atletismo marcha atletica prata",
+    },
+    {
+        titulo: "Willian Lima",
+        descricao: "Willian Lima, judoca brasileiro, trouxe mais uma medalha para o país, conquistando a prata na categoria -66kg em Paris 2024.",
+        link: "https://www.cob.org.br/time-brasil/medalhistas-olimpicos/willian-lima",
+        tags: "judô judoca prata",
+    },
 ];
